c9aeb54cb4e65481034d842108c0d2f065384b80
2018-08-22 <Emma Mask>
2019-10-29 11:16:04 -0500

    in class october 29

--- src/main/scala/adt/UnsortedLinkedList.scala
+++ src/main/scala/adt/UnsortedLinkedList.scala
@@ -1,14 +1,40 @@
 package adt
 
 class UnsortedLinkedList[A](higherP: (A, A) => Boolean) extends MyPriorityQueue[A] {
+    private val end: Node[A] = new Node[A](null, null.asInstanceOf[A], null)    // sentinel
+    end.prev = end
+    end.next = end
+    private var numElems = 0
+
     def enqueue(value: A): Unit = {
-        var rove
+        val n = new Node[A](end.prev, a, end)
+        end.prev.next = n
+        end.prev = n
+        numElems += 1
+        this
     }
-    def dequeue(): A
+
+    def dequeue(): A = {
+        
+    }
+
     def peek(): A
     def isEmpty: Boolean
+
+    def findHighestPriority(): Node = {
+        val rover = end.next
+        val tempNode = rover
+        while (rover != end) {
+            if(tempNode.data >= rover.next.data) {
+                rover = rover.next
+            } else {
+                val tempNode = rover.next
+                rover = rover.next
+            }
+        }
+    }
 }
 
 object UnsortedLinkedList {
-    private class Node(var end: Node[A], var data: A, var front: Node[A])
+    private class Node(var prev: Node[A], var data: A, var next: Node[A])
 }
