ca670568c5bd19b809403b9797f8f4d7f3cc62e1
2018-08-22 <Emma Mask>
2019-11-05 11:05:10 -0600

    added shortestPath to Graphs.scala, 11/5/19

--- src/main/scala/adt/Graphs.scala
+++ src/main/scala/adt/Graphs.scala
@@ -29,6 +29,27 @@ object Graphs extends App {
         helper(node1, Set.empty)
     }
 
+    def shortestPath(node1: Int, node2: Int, connect: Array[Array[Int]]): Int = {
+        // MUD will have shortest path, not reachable
+        def helper(n1: Int, visited: Set[Int]): Int = {
+            // nodes in MUD would be passed around as Strings for the keywords
+            if (node1 == node2) 0 else {
+                val ret = 1000000000
+                val newVisited = visited + n1
+                for (n <- 0 until connect.length) {
+                    if (connect(node1)(n) != 0 && !visited(n)) {
+                        ret = ret min helper(n, newVisited) // makes ret equal to the smaller of the two
+                    }
+                }
+                ret + 1
+            }
+        }
+        helper(node1, Set.empty)
+    }
+
     println(reachable(0, 4, g))
     println(reachable(0, 5, g))
+    println(shortestPath(0, 4, g))
+    println(shortestPath(0, 5, g))
+    println(shortestPath(5, 0, g))
 }
